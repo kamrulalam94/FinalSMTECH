ee44c67f56366c05e88217152bf54b8656b7a052
kamrulalam94 <alam.kamrul94@gmail.com>
2020-04-20 01:02:04 -0400

    04202020-1.00AM

--- src/test/java/runners/FinalRunner.java
+++ src/test/java/runners/FinalRunner.java
@@ -7,11 +7,10 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features = "src/test/resources/Finalproject.feature",
-		glue= {"final_project"},
-		tags = {"@001"},
+		glue= {"final_project"}, //stepDef
+		tags = {"@001"}, //featurefile anotation
 		plugin = {"com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html"})
- 
-
+ 			//plugin is where is save the report...
 public class FinalRunner {
 	
 	}
